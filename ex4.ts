ee60f4676b25e7204c0f896fabb92801d677ff40
HilaCohenn <cohenhila10@gmail.com>
2025-06-29 11:35:18 +0300

    q2

--- ex4.ts
+++ ex4.ts
@@ -29,12 +29,27 @@ export function all<T>(promises : Array<Promise<T>>) : Promise<Array<T>> {
   
 // Q2
 export function* Fib1() {
-	// @TODO
-  return undefined;
+  // Recurrence relation: F1 = 1, F2 = 1, Fn = Fn-1 + Fn-2
+  let a = 1, b = 1;
+  yield a; // F1
+  yield b; // F2
+  while (true) {
+    const next = a + b;
+    yield next;
+    a = b;
+    b = next;
+  }
 }
 
-
 export function* Fib2() {
-	// @TODO
-  return undefined;
+  // Binet's formula: F(n) = (phi^n - psi^n) / sqrt(5)
+  const sqrt5 = Math.sqrt(5);
+  const phi = (1 + sqrt5) / 2;
+  const psi = (1 - sqrt5) / 2;
+  let n = 1;
+  while (true) {
+    // Round to nearest integer to avoid floating point errors
+    yield Math.round((Math.pow(phi, n) - Math.pow(psi, n)) / sqrt5);
+    n++;
+  }
 }
